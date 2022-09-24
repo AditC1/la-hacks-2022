c687a9ee9e67fd55bb50d848feb781df8489e5ec
CarterC <pcdcmcccac@gmail.com>
2022-09-24 14:07:17 -0700

    making task functions in Todolist.sol

--- blockchain/contracts/Todolist.sol
+++ blockchain/contracts/Todolist.sol
@@ -4,6 +4,20 @@ contract TodoList {
     uint public taskCount = 0;
 
     struct Task{
-        
+        uint id;
+        string content;
+        bool completed;
     }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("test task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] =  Task(taskCount, _content, false);
+    }
+
 }
